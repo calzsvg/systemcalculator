37247dd1dfef1986cb4429354c42d2b4206f9b10
Musalar <pjs14344@naver.com>
2025-12-20 23:21:01 +0900

    후위식 수정

--- infix_to_postfix2.c
+++ infix_to_postfix2.c
@@ -1,154 +1,5 @@
 #include "header.h"
 
-struct datas
-{
-    enum {operator, value} kind;
-    union
-    {
-        char op;
-        int val;
-    } uni;   
-};
-typedef struct datas datas;
-
-struct elem
-{
-    datas    d;
-    struct elem *next;
-};
-typedef struct elem elem;
-
-struct stack
-{
-    int cnt;
-    elem *top;
-};
-typedef struct stack stack;
-typedef enum {false, true} boolean;
-
-void    initialize(stack *stk);
-void    push(datas d, stack *stk);
-datas   pop(stack *stk);
-boolean empty(const stack *stk);
-int     priority(char op);
-void    infix_to_postfix(const char *expr);
-
-int main()
-{
-    const char *expression = "16 + 34 * 57 / 6";    /*나중에는 수식을 작성해서 scanf로 받게끔 만들어야함*/
-    
-    printf("수식(infix): %s\n", expression);
-    printf("변경 후 수식(postfix): ");
-    infix_to_postfix(expression);
-    printf("\n");
-    
-    return 0;
-}
-
-void initialize(stack *stk)
-{
-    stk -> cnt = 0;
-    stk -> top = NULL;
-}
-
-void push(datas d, stack *stk)
-{
-    elem *pushnode;
-    pushnode = malloc(sizeof(elem));
-
-    pushnode -> d = d;
-    pushnode -> next = stk -> top;
-    stk -> top = pushnode;
-    stk -> cnt++;
-}
-
-datas pop(stack *stk)
-{
-    datas data;
-    elem *popnode;
-
-    popnode = stk -> top;
-    data = stk -> top -> d;
-    stk -> top = stk -> top -> next;
-    stk -> cnt--;
-    
-    free(popnode);
-    return data;
-}
-
-boolean empty(const stack *stk)
-{
-    return (stk -> cnt == 0) ? true : false;    /*if문 짧게 쓴거*/
-}
-
-int priority(char op)
-{
-    switch (op) {
-        case '*': 
-        case '/': 
-            return 2;
-        case '+': 
-        case '-': 
-            return 1;
-        default:  
-            return 0;       /*연산자 우선순위 부여*/
-    }
-}
-
-void infix_to_postfix(const char *str)
-{
-    stack op_stack;
-    initialize(&op_stack);
-    
-    const char *p = str;    /*입력 받은 수식*/
-    datas d;
-
-    while (*p != '\0')
-    {
-        if (isspace(*p))    /*공백*/
-        {
-            p++;
-            continue;
-        }
-
-        if (isdigit(*p))    /*숫자*/
-        {
-            int num = 0;
-            while (isdigit(*p))
-            {
-                num = num * 10 + (*p - '0');    /*문자열 숫자 -> int, 아스키코드를 이용*/
-                p++;
-            }
-            printf("%d ", num); 
-            continue; 
-        }
-
-        else if (*p == '+' || *p == '-' || *p == '*' || *p == '/')  /*연산자*/
-        {
-            while (!empty(&op_stack) && priority(op_stack.top->d.uni.op) >= priority(*p))   /*현재 연산자 >= 맨 위 연산자*/
-            {
-                datas popped = pop(&op_stack);
-                printf("%c ", popped.uni.op);   /*연산자 출력*/
-            }
-
-            d.kind = operator;
-            d.uni.op = *p;
-            push(d, &op_stack);
-            p++;
-        }
-        else {
-            p++;    /*숫자, 연산자를 제외한 나머지 문자: pass*/
-        }
-    }
-
-    while (!empty(&op_stack)) {
-        datas popped = pop(&op_stack);
-        printf("%c ", popped.uni.op);   /*연산자 출력2*/
-    }
-}
-
-#include "header.h"
-
 int priority(char op) 
 {
         switch (op) 
@@ -164,28 +15,21 @@ int priority(char op)
         }
 }
 
-// 후위 표기법 리스트를 가지고 실제 계산 수행
-Node* evaluatePostfix(Token* postfixHead)
+Token* infix_postfix(Token* infixTokens)
 {
-    // Node*를 저장하는 스택 필요 (계산 결과들을 담아둠)
-    // 1. 숫자를 만나면 스택에 push
-    // 2. 연산자를 만나면 스택에서 숫자(Node*) 2개를 pop
-    // 3. 연산 수행 (add, sub 등) 후 결과를 다시 스택에 push
-}
-
-Token* infixToPostfixTokens(Token* infixTokens) {
     Token* postfixHead = newTokenList();
     Stack opStack;
     initStack(&opStack);
 
     Token* curr = infixTokens->next; /*헤더 다음부터 시작*/
-    while (curr != NULL) {
+    while (curr != NULL)
+    {
         if (curr->type == 'N') /*숫자, parser.c의 타입 정의를 가져옴*/
         {
             addNumberToken(postfixHead, curr->number_head);
         }
         
-        else if (curr->op == '(')
+        else if (curr->op == '(')   /*괄호 반영*/
         {
             push(&opStack, '(');
         }
@@ -196,12 +40,12 @@ Token* infixToPostfixTokens(Token* infixTokens) {
             {
                 addOperatorToken(postfixHead, pop(&opStack));
             }
-            pop(&opStack); // '(' 제거
+            pop(&opStack);  /*'(' 를 꺼냄*/
         }
         
         else /*연산자*/
         {
-            while (!isEmpty(&opStack) && priority(peek(&opStack)) >= priority(curr->op))    /*연산자 우선순위 고려*/
+            while (!isEmpty(&opStack) && priority(peek(&opStack)) >= priority(curr->op))    /*연산자 우선순위 고려, */
             {
                 addOperatorToken(postfixHead, pop(&opStack));
             }
@@ -209,8 +53,11 @@ Token* infixToPostfixTokens(Token* infixTokens) {
         }
         curr = curr->next;
     }
-    while (!isEmpty(&opStack)) {
+
+    while (!isEmpty(&opStack))
+    {
         addOperatorToken(postfixHead, pop(&opStack));
     }
+
     return postfixHead;
 }
